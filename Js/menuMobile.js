b15bcc1899ba4c012a7a3971e815e020089686c2
tiago <tiagoandroloko@gmail.com>
2023-08-21 19:42:32 -0300

    Media query finalizadas

---
+++ Js/menuMobile.js
@@ -0,0 +1,12 @@
+const menuNav = document.getElementById('menu-nav');
+
+const btn = document.querySelector('.menu-mobile');
+
+//adicionar a funcao de clique no botao
+
+
+function abreMenu() {
+    menuNav.classList.toggle('active');
+}
+
+btn.addEventListener('click', abreMenu);
